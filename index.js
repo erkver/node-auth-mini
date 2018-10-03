5a0348276a2ebbb463933de075d0ccae938a5c6e
Eric Vere <erkver250@gmail.com>
2018-10-03 13:59:44 -0500

    complete node-auth-mini project

--- index.js
+++ index.js
@@ -1,7 +1,47 @@
+require ('dotenv').config();
+
 const express = require('express');
 const session = require('express-session');
+const passport = require('passport');
+const strategy = require('./strategy');
 
 const app = express();
 
-const port = 3000;
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 10000
+  }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(strategy);
+
+
+//generates user object for us to use
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+app.get('/login', passport.authenticate('auth0', {
+  successRedirect: '/me',
+  failureRedirect: '/login'
+}));
+
+app.get('/me', (req, res) => {
+  if(!req.user) {
+    res.sendStatus(401);
+  } else {
+    res.send(200).send(req.user);
+  }
+});
+
+const port = 3001;
 app.listen( port, () => { console.log(`Server listening on port ${port}`); } );
